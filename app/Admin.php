fb1cce506780beb7c670d67594c0fa846ae1c742
=quoccuong5435 <=>
2020-04-12 03:54:58 +0700

    update

--- app/Admin.php
+++ app/Admin.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Staff extends Model
+class Admin extends Model
 {
     //
 }
--- app/Http/Controllers/Booking_Controller.php
+++ app/Http/Controllers/Booking_Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ServiceController extends Controller
+class Booking_Controller extends Controller
 {
     /**
      * Display a listing of the resource.
